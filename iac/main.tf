d2a128357a850be0493812ddbd8c6ed882843c3e
Felipe Vilarinho <fvilarinho@users.noreply.github.com>
2025-02-20 09:26:13 -0300

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,16 +1,5 @@
 # Terraform definition.
 terraform {
-  # Stores the provisioning state in Akamai Cloud Computing Object Storage (Please change to use your own).
-  backend "s3" {
-    bucket                      = "fvilarin-devops"
-    key                         = "akamai-grafana-loki.tfstate"
-    region                      = "us-east-1"
-    endpoint                    = "us-east-1.linodeobjects.com"
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_metadata_api_check     = true
-  }
-
   required_providers {
     linode = {
       source = "linode/linode"
